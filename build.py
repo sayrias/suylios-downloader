6b1b8ede1a9528650ed96319c77d1e3e7953349a
sayrias <erentrk.80@gmail.com>
2026-07-08 22:18:33 +0300

    v1.3.6 - Updater System Overhaul & UI Fixes

--- build.py
+++ build.py
@@ -120,8 +120,8 @@ Source: "{staging_dir}\bin\*"; DestDir: "{{app}}\bin"; Flags: ignoreversion recu
 Source: "{staging_dir}\*"; DestDir: "{{app}}"; Flags: ignoreversion recursesubdirs createallsubdirs skipifsourcedoesntexist
 
 [INI]
-Filename: "{app}\install_lang.ini"; Section: "Setup"; Key: "Language"; String: "tr"; Languages: turkish
-Filename: "{app}\install_lang.ini"; Section: "Setup"; Key: "Language"; String: "en"; Languages: english
+Filename: "{{app}}\install_lang.ini"; Section: "Setup"; Key: "Language"; String: "tr"; Languages: turkish
+Filename: "{{app}}\install_lang.ini"; Section: "Setup"; Key: "Language"; String: "en"; Languages: english
 
 [Icons]
 Name: "{{autoprograms}}\{app_name}"; Filename: "{{app}}\{app_exe}"; Tasks: startmenuicon
